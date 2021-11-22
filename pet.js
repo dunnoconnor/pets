c2f488917b7bacdcad636d438041ec78adeae6ea
dunnoconnor <moconnor8@gmail.com>
2021-11-22 14:42:02 -0500

    initial commit

---
+++ pet.js
@@ -0,0 +1,11 @@
+class Pet {
+    constructor(name,species,color){
+        this.name = name
+        this.species = species
+        this.color = color
+    }
+
+    changeName(name){
+        this.name = name
+    }
+}
